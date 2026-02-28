7af0c65bbeed4d91a162b502130be857ad5534fd
Jesse Soto <jesse@Jesses-MacBook-Air.local>
2026-02-28 11:24:42 -0800

    scripts

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("--Variables--");
+
+//string (text) ""
+let firstName = "Samantha"; // assign the value
+let myAge = 99;
+//boolean
+let isStudent = false;
+let isProfessor = true;
+
+console.log(firstName);
+console.log(myAge);
+console.log(isStudent);
+console.log(isProfessor);
+
+let lastName = "Soto";
+let heightInches = 68
+console.log(lastName);
+console.log(heightInches + "ft"); // conctenation
+
+// Arithmetic ops
+
+let num1 = 10;
+let num2 = 20;
+
+
+let sum = num1 + num2;
+//console.log(num1 + "+" + num2 + "=" + sum);
+console.log(`${num1} + ${num2} = ${sum}`);
+
+let r = 3;
+let pi = 3.1416;
+let area = pi * r * r;
+
+console.log(`${pi} * ${r} * ${r} = ${area}`);
+
+
+let aVariable = 40;
+aVariable = 50;
+aVariable = 60;
+
+console.log(aVariable);
+
+const weekDays = 7; // never change const. locked in
+//weekDays = 9; ERROR
