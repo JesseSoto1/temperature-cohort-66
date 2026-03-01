0fee5856f3c234ea7b23240dfc3674eddaba9917
Jesse Soto <jesse@Jesses-MacBook-Air.local>
2026-02-28 17:27:50 -0800

    funny-story

---
+++ assignment1.js
@@ -0,0 +1,23 @@
+console.log("--Variables--");
+
+let characterName = " Pancho ";
+let characterName2 = " Matilde ";
+let action = " talked about ";
+let object = " with a really shiny beach ball ";
+let place = " the cemetery ";
+let time = " the ugly evening ";
+
+const game = " playing catch ";
+
+console.log("-- Funny Story --");
+console.log(characterName + " was at " + place + " along with " + characterName2);
+console.log(" they " + action + " how " + time + " ruined " + game + object);
+
+characterName = " Mictlantecuhtli ";
+characterName2 = " Huitzilopochtli ";
+action = " argued over ";
+object = " bones ";
+place = " the mictlan ";
+time = " a dark eerie night";
+
+console.log(characterName + action + object + " with " + characterName2 + " at " + place + " on " + time);
